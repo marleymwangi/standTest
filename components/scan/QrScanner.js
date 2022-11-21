9cc7ba40035cb04168907775fe348543513eff1c
Bob Mwangi <bobmwangee@gmail.com>
2022-11-21 13:34:57 +0300

    barcode v.01.05

--- components/scan/QrScanner.js
+++ components/scan/QrScanner.js
@@ -1,34 +1,22 @@
-import React from "react";
 //custom
 import Html5QrcodePlugin from "./Html5QrcodePlugin";
 
-class QrScanner extends React.Component {
-  constructor(props) {
-    super(props);
-
-    // This binding is necessary to make `this` work in the callback.
-    this.onNewScanResult = this.onNewScanResult.bind(this);
-  }
-
-  render() {
-    return (
-      <div>
-        <h1>Html5Qrcode React example!</h1>
-        <Html5QrcodePlugin
-          fps={10}
-          qrbox={250}
-          disableFlip={false}
-          qrCodeSuccessCallback={this.onNewScanResult}
-        />
-      </div>
-    );
-  }
-
-  onNewScanResult(decodedText, decodedResult) {
+export default function QrScanner() {
+  const onNewScanResult = (decodedText, decodedResult) => {
     // Handle the result here.
     console.log("decodedResult ", decodedResult);
     console.log("decodedText ", decodedText);
-  }
-}
+  };
 
-export default QrScanner;
+  return (
+    <div>
+      <h1>Html5Qrcode React example!</h1>
+      <Html5QrcodePlugin
+        fps={10}
+        qrbox={250}
+        disableFlip={false}
+        qrCodeSuccessCallback={onNewScanResult}
+      />
+    </div>
+  );
+}
